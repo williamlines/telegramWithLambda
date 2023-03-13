4bc6d3b915ad5cc9f068ba00f5519b6aaf41c24d
William Lines <williamlines42@gmail.com>
2023-03-13 15:32:41 +0000

    started building out sending a message to user

--- handler.js
+++ handler.js
@@ -1,7 +1,10 @@
 "use strict";
 const TELEGRAM_HELLO_WORLD_KEY = process.env.TELEGRAM_HELLO_WORLD_KEY;
 
-const sendMessage = async (msg) => {};
+const sendMessage = async (chatID, msg) => {
+  const url = `https://api.telegram.org/bot${TELEGRAM_HELLO_WORLD_KEY}/sendMessage?chat_id=${chatID}&text=${msg}`;
+  fetch(url);
+};
 
 module.exports.helloWorld = async (event) => {
   const body = event.body;
@@ -11,6 +14,9 @@ module.exports.helloWorld = async (event) => {
   console.log(bodyData);
 
   const chatID = bodyData.message.chat.id;
+
+  await sendMessage(chatID, "Hello World!");
+
   return {
     statusCode: 200,
   };
