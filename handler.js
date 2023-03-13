589cc91d538f346813b271b04ff4826e9fede9d0
William Lines <williamlines42@gmail.com>
2023-03-13 15:18:13 +0000

    added environment variables

--- handler.js
+++ handler.js
@@ -1,12 +1,12 @@
 "use strict";
+const TELEGRAM_HELLO_WORLD_KEY = process.env.TELEGRAM_HELLO_WORLD_KEY;
 
 const sendMessage = async (msg) => {};
 
 module.exports.helloWorld = async (event) => {
-  
   const body = event.body;
   const bodyData = JSON.parse(body);
-  
+
   console.log("this is the body data");
   console.log(bodyData);
 
