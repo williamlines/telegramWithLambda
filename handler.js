ce96e6278cdd14a9c5db64bb6c831c5f90e78b79
William Lines <williamlines42@gmail.com>
2023-03-31 12:34:15 +0100

    removed comments

--- handler.js
+++ handler.js
@@ -4,8 +4,6 @@ const TELEGRAM_HELLO_WORLD_KEY = process.env.TELEGRAM_HELLO_WORLD_KEY;
 const sendMessage = async (chatID, msg) => {
   const url = `https://api.telegram.org/bot${TELEGRAM_HELLO_WORLD_KEY}/sendMessage?chat_id=${chatID}&text=${msg}`;
   const res = await fetch(url);
-  console.log("fetch request made! Response:");
-  console.log(res);
 };
 
 module.exports.helloWorld = async (event) => {
